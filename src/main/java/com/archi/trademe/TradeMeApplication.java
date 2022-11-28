f556cc946f6d14ed8d429a5a4af2331f5f1b51e0
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-11-28 14:46:50 +0100

    Init project

---
+++ src/main/java/com/archi/trademe/TradeMeApplication.java
@@ -0,0 +1,13 @@
+package com.archi.trademe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/archi/trademe/TradeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archi.trademe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
